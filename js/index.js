191fc1d13e018ad48621b25c12df62ee0f8f1d0b
Tomáš Boďa <tominoboda@gmail.com>
2020-08-18 11:30:44 +0200

    reduced number of css and js files

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
+function showImage(image) {
+    var path = "assets/" + image + ".jpg";
+    
+    document.getElementById("gallery-image").src = path;
+    document.getElementById("gallery-image-container").style.display = "flex";
+}
+
+function hideImage() {
+    document.getElementById("gallery-image-container").style.display = "none";
+}
+
+function showMenu() {
+    var height = document.getElementById("mobile-menu-content").offsetHeight;
+    
+    var menu = document.getElementById("mobile-menu");
+    menu.style.height = height + "px";
+    //menu.style.maxHeight = "300px";
+    
+    document.getElementById("button").setAttribute("onclick", "javascript:hideMenu();");
+}
+
+function hideMenu() {
+    var menu = document.getElementById("mobile-menu");
+    menu.style.height = "0px";
+    
+    document.getElementById("button").setAttribute("onclick", "javascript:showMenu();");
+}
+
 function nextSlide() {
     var slideshow = document.getElementById("slides");
     
